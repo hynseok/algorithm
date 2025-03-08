310448ed2fd594ddc5178bc7f6f0f3d30680e3af
김현석 <wefwef12e@naver.com>
2025-03-09 00:36:49 +0900

    [level 1] Title: 폰켓몬, Time: 0.58 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 int solution(vector<int> nums)
 {
-    int answer = 0;
     int len = nums.size()/2;
     sort(nums.begin(), nums.end());
     nums.erase(unique(nums.begin(), nums.end()), nums.end());
-    
-    answer = nums.size() > len ? len : nums.size();
      
-    return answer;
+    return nums.size() > len ? len : nums.size();
 }
